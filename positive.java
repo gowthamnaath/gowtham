3dc6a166bb4d39a2271296c32f23dff63ee5baae
gowthamnaath <gowthamnaath@gmail.com>
2017-02-12 12:49:04 +0530

    positive
    positive or negative

---
+++ positive.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class positive {
+
+	public static String checkSignWithRelational(double number){ {
+		// TODO Auto-generated method stub
+double number= 9;
+System.out.println(+number);
+{
+	if( number < 0){
+        return "negative";
+    }else {
+        return "positive";
+    }
+
+
+		
+}
+	}
+
+	}
+}
