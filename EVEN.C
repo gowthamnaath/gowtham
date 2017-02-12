03980fa7d9344665a06c437f7d0bc0f4c45e2d16
gowthamnaath <gowthamnaath@gmail.com>
2017-02-12 16:09:39 +0530

    Add files via upload

---
+++ EVEN.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+clrscr();
+printf("find in even or odd");
+printf("\n enter the num");
+scanf("%d",num);
+if(num%2==0)
+{
+printf("this is even num");
+}
+else
+{printf("this is odd num");
+}
+}
